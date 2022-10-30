6b2f15ea8510e0f0ebcbe4a0875d6b305027c29a
Sergio1706 <sergiodanielexner@gmail.com>
2022-10-30 20:56:32 -0300

    test terminados

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -147,7 +147,7 @@ function agregarMetodoCalculoDescuento (producto) {
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
   producto['calcularPrecioDescuento'] = function(){
-    return this.precio - this.precio * this.porcentajeDeDescuento
+    return (this.precio - this.precio * this.porcentajeDeDescuento)
   }
     return producto;
 
